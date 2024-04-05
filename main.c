2f9f5e41b01443f2f6bdc516b980101860f2829b
Ow Kai Ren <65292641+KuroZenon@users.noreply.github.com>
2024-04-05 21:49:57 +0800

    Added menu

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-int main() {
+int overFlowDemo(){
     int64_t x1 = INT64_MAX;
     int64_t x2 = INT64_MAX; 
     int64_t wallet1 = 0;
@@ -11,7 +11,7 @@ int main() {
    // printf("Initial value of x: %d\n", x);
     int64_t UTXO = 50;
     int64_t final_value = UTXO - (x1 + x2);
-    if ( final_value >= 0)
+    if (final_value >= 0)
     {
         printf("bitcoin allocated to wallet 1: %"PRId64"\n", x1);
         printf("bitcoin allocated to wallet 2: %"PRId64"\n", x2);
@@ -24,4 +24,32 @@ int main() {
         printf("%d Error, bitcoin not allocated", final_value);
 
     return 0;
+}
+
+int main() {
+    int user_input;
+    while(1)
+    {
+        printf("1: Overflow demo\n2: Fix Demo\n3: Exit\n");
+        scanf("%d", &user_input);
+        if (user_input == 1)
+        {
+            printf("Overflow demo\n");
+            overFlowDemo();
+        }
+        else if (user_input == 2)
+        {
+            printf("Fix demo\n");
+        }
+        else if (user_input == 3)
+        {
+            printf("Exiting\n");
+            break;
+        }
+        else
+        {
+            printf("Invalid input\n");
+        }
+    }
+   
 }
